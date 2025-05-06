67b01d83ea7d2ec5a5fc7c88de4cd522301ff6f7
Quốc Huy <79572016+vdqhuy@users.noreply.github.com>
2025-05-06 15:52:07 +0700

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,7 +1,7 @@
 // db.js
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('tiramisu', 'admin', 'admindb12345!', {
+const sequelize = new Sequelize('tiramisu', 'admin', 'admindb12345', {
   host: 'tiramisu.ckfu67ytk3xo.ap-southeast-1.rds.amazonaws.com',
   dialect: 'mariadb',
   logging: true
