ac724fe938e03c9d334bcefeb0eca5f977285d94
vdqhuy <vdqhuy99@gmail.com>
2025-05-10 22:24:06 +0700

    fix: can't get today's login histories due to conflict routes

--- backend/routes/loginHistoryRoutes.js
+++ backend/routes/loginHistoryRoutes.js
@@ -3,10 +3,11 @@ const router = express.Router();
 const controller = require('../controllers/loginHistoryController');
 
 router.get('/', controller.getAll);
+router.get('/today', controller.getTodayHistory);
+router.get('/today/:login_id', controller.getTodayHistory);
 router.get('/:login_id', controller.getByLoginId);
 
 router.post('/add-login-history', controller.addLoginHistory);
   
-router.get('/today/:login_id', controller.getTodayHistory);
 
 module.exports = router;
